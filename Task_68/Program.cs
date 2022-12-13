d56d4108aa1ee2e19a286d4146b3a35f1cc4eadf
Tatiana443 <tvb399@yandex.ru>
2022-12-13 15:24:58 +0600

    Project Task68

--- Task_68/Program.cs
+++ Task_68/Program.cs
@@ -1,13 +1,13 @@
-﻿Console.WriteLine("Введите  число M: ");
+﻿Console.WriteLine("Введите  неотрицательное целое число M: ");
 int numM = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите натуральное число N: ");
+Console.WriteLine("Введите неотрицательное целое число N: ");
 int numN = Convert.ToInt32(Console.ReadLine());
 
 int FunctionAkkerman(int numM, int numN)
 {
-    if (numM == 0) return numN +1;
-    else if (numM > 0 && numN == 0) return FunctionAkkerman(numM - 1, 1);
-    else return FunctionAkkerman(numM - 1, FunctionAkkerman(numM, numN - 1));
+    if (numM == 0) return numN + 1;
+    if (numM > 0 && numN == 0) return FunctionAkkerman(numM - 1, 1);
+    return FunctionAkkerman(numM - 1, FunctionAkkerman(numM, numN - 1));
 }
 int functionAkkerman = FunctionAkkerman(numM, numN);
 Console.WriteLine($"A(m,n) = {functionAkkerman}");
