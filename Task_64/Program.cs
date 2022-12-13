3b9c2f11a3e4b2f5f1f73bfdf91490af0c5bb66c
Tatiana443 <tvb399@yandex.ru>
2022-12-13 10:53:18 +0600

    Project Task64

---
+++ Task_64/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+NaturalNumbers(number);
+void NaturalNumbers(int num)
+{
+    if (num == 0) return;
+    Console.Write($"{num}, ");
+    NaturalNumbers(num - 1);
+}
